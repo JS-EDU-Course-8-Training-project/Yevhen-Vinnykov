39c469bdf211eb0bb179831e5eddcb7e9b3a223d
Yevhen Vinnykov <yevhen.vinnykov@dev.pro>
2022-04-25 17:18:46 +0200

    JEC8-147 - Refactor Article List Component

--- src/app/components/article-list/article-list.component.ts
+++ src/app/components/article-list/article-list.component.ts
@@ -2,6 +2,7 @@ import { IArticle } from '../../models/IArticle';
 import { Component, Input, OnInit } from '@angular/core';
 import { ArticlesService } from 'src/app/services/articles.service';
 import { Router } from '@angular/router';
+import { AuthorizationService } from 'src/app/services/authorization.service';
 
 @Component({
   selector: 'app-article-list',
@@ -9,37 +10,48 @@ import { Router } from '@angular/router';
   styleUrls: ['./article-list.component.scss']
 })
 export class ArticleListComponent implements OnInit {
-  isLiked!: boolean;
-  favoriteInProgress: boolean = false;
-  likesCount!: number;
   @Input() article!: IArticle;
-  constructor(private articlesService: ArticlesService, private router: Router) {}
+  public isLiked!: boolean;
+  public isPending: boolean = false;
+  public likesCount!: number;
+  private isAuthorized!: boolean;
 
-  handleLike(slug: string): void {
-    if(localStorage.getItem('authorized') !== 'true'){
-      this.router.navigateByUrl('/sign-in').catch((err: any) => console.log(err));
-      return;
-    }
-    this.favoriteInProgress = true;
-    if(this.isLiked){
-      this.articlesService.removeFromFavorites(slug).subscribe(article => {
-        this.isLiked = article.favorited;
-        this.likesCount = article.favoritesCount;
-        this.favoriteInProgress = false;
-        
-      })
-    } else {
-      this.articlesService.addToFavorites(slug).subscribe(article => {
-        this.isLiked = article.favorited;
-        this.favoriteInProgress = false;
-        this.likesCount = article.favoritesCount;
-      })
-    }
-  }
+  constructor(
+    private articlesService: ArticlesService,
+    private router: Router,
+    private authorizationService: AuthorizationService
+  ) { }
 
   ngOnInit(): void {
-   this.likesCount = this.article.favoritesCount;
-   this.isLiked = this.article.favorited;
+    this.likesCount = this.article.favoritesCount;
+    this.isLiked = this.article.favorited;
+    this.authorizationService.isAuthorized$.subscribe(isAuthorized => this.isAuthorized = isAuthorized);
   }
 
+  public handleLikeDislike(slug: string): void {
+    if((!this.isAuthorized)) return this.redirectUnauthorized();
+    this.isPending = true;
+    if (this.isLiked) return this.dislikeArticle(slug);
+    if (!this.isLiked) return this.likeArticle(slug);
+  }
+
+  private likeArticle(slug: string): void {
+    this.articlesService.addToFavorites(slug).subscribe(article => {
+      this.isLiked = article.favorited;
+      this.isPending = false;
+      this.likesCount = article.favoritesCount;
+    })
+  }
+
+  private dislikeArticle(slug: string): void {
+    this.articlesService.removeFromFavorites(slug).subscribe(article => {
+      this.isLiked = article.favorited;
+      this.likesCount = article.favoritesCount;
+      this.isPending = false;
+    })
+  }
+
+  private redirectUnauthorized(): void {
+    this.router.navigateByUrl('/sign-in').catch((err: any) => console.log(err));
+  }
 }
