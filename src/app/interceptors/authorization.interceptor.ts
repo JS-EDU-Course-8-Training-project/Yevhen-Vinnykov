a686ad7c7e44b2098743cd6f7dacae21eaabc408
Yevhen Vinnykov <yevhen.vinnykov@dev.pro>
2022-04-26 15:14:22 +0200

    JEC8-147 - Refactor Authorization Interceptor

--- src/app/interceptors/authorization.interceptor.ts
+++ src/app/interceptors/authorization.interceptor.ts
@@ -5,19 +5,20 @@ import {
   HttpEvent,
   HttpInterceptor
 } from '@angular/common/http';
-import { Observable, BehaviorSubject } from 'rxjs';
+import { Observable } from 'rxjs';
 import { AuthorizationService } from '../services/authorization.service';
 
 @Injectable()
 export class AuthorizationInterceptor implements HttpInterceptor {
   private isAuthorized: boolean = false;
+
   constructor(
     readonly authorizationService: AuthorizationService
   ) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     this.authorizationService.checkIfAuthorized();
-    this.authorizationService.isAuthorized$.subscribe(isAuthorized => this.isAuthorized = isAuthorized);
+    this.isAuthorized = this.authorizationService.isAuthorized$.getValue();
       if(this.isAuthorized){
         const token: string = `Bearer ${localStorage.getItem('token')}`;
         const authorizedRequest = request.clone({
