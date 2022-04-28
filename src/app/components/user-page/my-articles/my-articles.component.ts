84f68f2ae158abfa4268b7caef49df173e17e4e9
Yevhen Vinnykov <yevhen.vinnykov@dev.pro>
2022-04-28 13:17:07 +0200

    JEC8-160 - Add Infinite Scroll to My Articles

--- src/app/components/user-page/my-articles/my-articles.component.ts
+++ src/app/components/user-page/my-articles/my-articles.component.ts
@@ -1,29 +1,42 @@
-import { Subject, takeUntil } from 'rxjs';
+import { Subject, takeUntil, BehaviorSubject } from 'rxjs';
 import { ArticlesService } from 'src/app/shared/services/articles/articles.service';
 import { IArticle } from 'src/app/shared/models/IArticle';
-import { Component, Input, OnChanges, OnDestroy } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, ViewChildren, ElementRef, QueryList, AfterViewInit } from '@angular/core';
 import { HttpErrorResponse } from '@angular/common/http';
+import { InfiniteScrollService } from 'src/app/shared/services/infinite-scroll/infinite-scroll.service';
 
 @Component({
   selector: 'app-my-articles',
   templateUrl: './my-articles.component.html',
   styleUrls: ['./my-articles.component.scss']
 })
-export class MyArticlesComponent implements OnChanges, OnDestroy {
+export class MyArticlesComponent implements OnChanges, OnDestroy, AfterViewInit {
+  @ViewChildren('lastItem', { read: ElementRef }) lastItem!: QueryList<ElementRef>;
+
   @Input() username!: string;
   @Input() tabIndex!: number;
 
   public myArticles: IArticle[] = [];
   public isLoading: boolean = false;
   private notifier: Subject<void> = new Subject<void>();
+  public isFinished!: boolean;
+  private offset: number = 0;
+  private pagesTotalCount!: number;
+  private limit: number = 5;
+  private currentPage: number = 1;
+  public canLoad$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
 
   constructor(
     private articlesService: ArticlesService,
+    private infiniteScroll: InfiniteScrollService
   ) { }
 
   ngOnChanges(): void {
+    this.reset();
     if (this.tabIndex === 0) {
       this.getArticles();
+      this.infiniteScroll
+        .observeIntersection({ canLoad: this.canLoad$, callback: this.getArticles.bind(this) });
     }
   }
 
@@ -32,15 +45,41 @@ export class MyArticlesComponent implements OnChanges, OnDestroy {
     this.notifier.complete();
   }
 
+  ngAfterViewInit(): void {
+    this.lastItem.changes.subscribe(change => {
+      if (change.last) this.infiniteScroll.observer.observe(change.last.nativeElement);
+    });
+  }
+
   private getArticles(): void {
     this.isLoading = true;
-    this.articlesService.fetchUserArticles(this.username)
+    this.articlesService.fetchUserArticles(this.username, this.limit, this.offset)
       .pipe(takeUntil(this.notifier))
       .subscribe(res => {
         if (!(res instanceof HttpErrorResponse)) {
-          this.myArticles = res.articles;
+          this.myArticles = [...this.myArticles, ...res.articles];
           this.isLoading = false;
+          this.pagesTotalCount = Math.ceil(res.articlesCount / this.limit);
+          this.isFinished = this.currentPage === this.pagesTotalCount;
+          this.isLoading = false;
+          this.canLoad$.next(!this.isFinished && !this.isLoading);
+          this.nextPage();
         }
       });
+  }
+
+  private nextPage() {
+    if (this.currentPage < this.pagesTotalCount) {
+      this.currentPage++;
+      this.offset += this.limit;
+    }
+  }
+
+  private reset(): void {
+    this.myArticles = [];
+    this.offset = 0;
+    this.isFinished = false;
+    this.currentPage = 1;
+    this.pagesTotalCount = 0;
   }
 }
