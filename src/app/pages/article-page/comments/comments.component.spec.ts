bfb74ce69746178f915d4da8413769530cd24ac2
Yevhen Vinnykov <yevhen.vinnykov@dev.pro>
2022-05-02 17:00:25 +0200

    JEC8-169 - Add Tests for Comments Component WIP

--- src/app/pages/article-page/comments/comments.component.spec.ts
+++ src/app/pages/article-page/comments/comments.component.spec.ts
@@ -1,6 +1,41 @@
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { IExistingUser } from './../../../shared/models/IExistingUser';
+import { Observable, of, Subject } from 'rxjs';
+import { IComment } from './../../../shared/models/IComment';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CommentsComponent } from './comments.component';
+import { CommentsService } from '../services/comments/comments.service';
+import { By } from '@angular/platform-browser';
+
+const comments: IComment[] = [{
+  id: 1,
+  createdAt: Date.now().toString(),
+  updatedAt: Date.now().toString(),
+  body: 'test-comment',
+  author: {
+    username: 'test-username',
+    bio: 'test-bio',
+    image: 'test-image',
+    following: false,
+  }
+}];
+
+const authUser: IExistingUser = {
+  email: 'test@example.com',
+  username: 'test-username',
+  bio: 'test-bio',
+  image: 'test-image',
+  token: 'test-token',
+  password: 'test-password'
+};
+
+
+class CommentsServiceMock {
+  public fetchArticleComments = (slug: string): Observable<IComment[]> => of(comments);
+  public removeComment = (slug: string, id: number): Observable<IComment> => of(comments[0]);
+}
 
 describe('CommentsComponent', () => {
   let component: CommentsComponent;
@@ -8,7 +43,11 @@ describe('CommentsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CommentsComponent ]
+      declarations: [ CommentsComponent ],
+      imports: [MatCardModule, MatIconModule],
+      providers: [
+        { provide: CommentsService, useClass: CommentsServiceMock }
+      ]
     })
     .compileComponents();
   });
@@ -16,10 +55,22 @@ describe('CommentsComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CommentsComponent);
     component = fixture.componentInstance;
+    component.slug = 'test-slug';
+    component.authUser = authUser;
+    component.requestForComments$ = new Subject<void>();
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should delete comment', () => {
+    fixture.detectChanges();
+    component.ngOnInit();
+    const deleteIcon = fixture.debugElement.query(By.css('[data-angular="test-delete-icon"]'));
+    deleteIcon.triggerEventHandler('click', null);
+    expect(deleteIcon.nativeElement).toBeTruthy();
+  });
+
 });
