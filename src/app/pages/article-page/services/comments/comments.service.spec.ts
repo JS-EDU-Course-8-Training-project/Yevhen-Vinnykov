10fc1dd04be4216a10f19852cc5f423a6cbcc3b0
Yevhen Vinnykov <yevhen.vinnykov@dev.pro>
2022-05-02 18:20:24 +0200

    JEC8-169 - Add Tests for Comments Service

--- src/app/pages/article-page/services/comments/comments.service.spec.ts
+++ src/app/pages/article-page/services/comments/comments.service.spec.ts
@@ -1,16 +1,63 @@
-import { TestBed } from '@angular/core/testing';
+import { IComment } from './../../../../shared/models/IComment';
+import { TestBed, waitForAsync } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { CommentsService } from './comments.service';
+
+const expectedData: IComment[] = [{
+  id: 1,
+  createdAt: Date.now().toString(),
+  updatedAt: Date.now().toString(),
+  body: 'test-comment',
+  author: {
+    username: 'test-username',
+    bio: 'test-bio',
+    image: 'test-image',
+    following: false,
+  }
+}];
 
 describe('CommentService', () => {
   let service: CommentsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ]
+    });
     service = TestBed.inject(CommentsService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('fetch correct comments', () => {
+    const spy = spyOn(service, 'fetchArticleComments').and.callThrough();
+    service.fetchArticleComments('test-slug').subscribe(comments => {
+      expect(comments).toEqual(expectedData);
+    });
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should create comment', () => {
+    const spy = spyOn(service, 'createComment').and.callThrough();
+    service.createComment('test-slug', { body: 'test-comment' }).subscribe(comments => {
+      expect(comments).toEqual(expectedData[0]);
+    });
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should delete comment', waitForAsync(() => {
+    const spy = spyOn(service, 'removeComment').and.callThrough();
+    service.removeComment('test-slug', 1).subscribe(comment => {
+      expect(comment).toEqual(expectedData[0]);
+    });
+    expect(spy).toHaveBeenCalled();
+  }));
+
 });
+
+
+
