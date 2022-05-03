cffcc1449bcbcb281dd2fdd68d8d3381f10308bb
Yevhen Vinnykov <yevhen.vinnykov@dev.pro>
2022-05-03 20:03:04 +0200

    JEC8-169 - Add Tests for Confirmation Guard

--- src/app/shared/guards/confirmation.guard.spec.ts
+++ src/app/shared/guards/confirmation.guard.spec.ts
@@ -1,16 +1,44 @@
+import { MatDialog } from '@angular/material/dialog';
 import { TestBed } from '@angular/core/testing';
 
 import { ConfirmationGuard } from './confirmation.guard';
 
+class DialogMock {
+  public open = () => ({
+    afterClosed: () => false
+  });
+}
+
+const componentStub = {
+  isDataSaved: () => true
+};
+
 describe('ConfirmationGuard', () => {
-  let guard: ConfirmationGuard;
+  let guard: any; //ConfirmationGuard;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        ConfirmationGuard,
+        { provide: MatDialog, useClass: DialogMock }
+      ]
+    });
     guard = TestBed.inject(ConfirmationGuard);
   });
 
   it('should be created', () => {
     expect(guard).toBeTruthy();
   });
+
+  it('should allow deactivation', () => {
+    guard.canDeactivate(componentStub).subscribe((res: boolean) => {
+      expect(res).toBe(true);
+    });
+  });
+
+  it('should not allow deactivation', () => {
+    const componentStub = { isDataSaved: () => false };
+    expect(guard.canDeactivate(componentStub)).toBe(false);
+  });
+
 });
