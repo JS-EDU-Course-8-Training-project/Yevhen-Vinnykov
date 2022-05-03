215414f2104af97fa8ecc4d325df807504a086d1
Yevhen Vinnykov <yevhen.vinnykov@dev.pro>
2022-05-03 19:35:12 +0200

    JEC8-169 - Add Tests for Authorization Guard

--- src/app/shared/guards/authorization.guard.spec.ts
+++ src/app/shared/guards/authorization.guard.spec.ts
@@ -1,16 +1,76 @@
-import { TestBed } from '@angular/core/testing';
+import { AuthorizationService } from './../services/authorization/authorization.service';
+import { of } from 'rxjs';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Router } from '@angular/router';
+import { TestBed, waitForAsync } from '@angular/core/testing';
 
 import { AuthorizationGuard } from './authorization.guard';
 
-describe('AuthorizationGuard', () => {
-  let guard: AuthorizationGuard;
+class AuthorizationServiceMockNotAuth {
+  public isAuthorized$ = of(false);
+}
+
+class AuthorizationServiceMock {
+  public isAuthorized$ = of(true);
+}
+
+describe('AuthorizationGuard Unauthorized', () => {
+  let guard: any;
+  let authorizationService: AuthorizationServiceMockNotAuth;
+  let routeMock: any = { snapshot: {} };
+  let routeStateMock: any = { snapshot: {}, url: '/create-article' };
+  let routerMock = { createUrlTree: jasmine.createSpy('navigate') }
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        AuthorizationGuard,
+        { provide: AuthorizationService, useClass: AuthorizationServiceMockNotAuth },
+        { provide: Router, useValue: routerMock }
+      ],
+      imports: [HttpClientTestingModule]
+    });
     guard = TestBed.inject(AuthorizationGuard);
+    authorizationService = TestBed.inject(AuthorizationService);
   });
 
   it('should be created', () => {
     expect(guard).toBeTruthy();
   });
+
+  it('should redirect an unauthorized user to the login route', waitForAsync(() => {
+    guard.canActivate(routeMock, routeStateMock).subscribe(() => {
+      expect(routerMock.createUrlTree).toHaveBeenCalledWith(['/sign-in']);
+    })
+  }));
+
+});
+
+
+describe('AuthorizationGuard Authorized', () => {
+  let guard: any;
+  let authorizationService: AuthorizationServiceMock;
+  let routeMock: any = { snapshot: {} };
+  let routeStateMock: any = { snapshot: {}, url: '/create-article' };
+  let routerMock = { createUrlTree: jasmine.createSpy('navigate') }
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        AuthorizationGuard,
+        { provide: AuthorizationService, useClass: AuthorizationServiceMock },
+        { provide: Router, useValue: routerMock }
+      ],
+      imports: [HttpClientTestingModule]
+    });
+    guard = TestBed.inject(AuthorizationGuard);
+    authorizationService = TestBed.inject(AuthorizationService);
+  });
+
+  it('should allow the authenticated user to access new article page', waitForAsync(() => {
+    guard.canActivate(routeMock, routeStateMock).subscribe((res: boolean) => {
+      expect(res).toBe(true);
+    });
+  }));
+
 });
