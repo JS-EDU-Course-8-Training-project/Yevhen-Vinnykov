c1dec8aae9e4ead4e770e59fd8f599a87dd3c4a1
Yevhen Vinnykov <yevhen.vinnykov@dev.pro>
2022-05-03 20:53:38 +0200

    JEC8-169 - Add Tests for Authorization Interceptor WIP

--- src/app/shared/interceptors/authorization.interceptor.spec.ts
+++ src/app/shared/interceptors/authorization.interceptor.spec.ts
@@ -1,16 +1,92 @@
-import { TestBed } from '@angular/core/testing';
+import { AuthorizationService } from './../services/authorization/authorization.service';
+import { of, BehaviorSubject } from 'rxjs';
+import { fakeAsync, TestBed, tick, waitForAsync } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AuthorizationInterceptor } from './authorization.interceptor';
+import { MockDataService } from './mock-data-service.service';
+
+class AuthorizationServiceMock {
+  public checkIfAuthorized = () => { }
+  public isAuthorized$ = new BehaviorSubject<boolean>(true);
+}
+
+class AuthorizationServiceMockNotAuth {
+  public checkIfAuthorized = () => { }
+  public isAuthorized$ = new BehaviorSubject<boolean>(true);
+}
+
 
 describe('AuthorizationInterceptor', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    providers: [
-      AuthorizationInterceptor
+  let interceptor: AuthorizationInterceptor;
+  let httpMock: HttpTestingController;
+  let service: MockDataService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        AuthorizationInterceptor,
+        MockDataService,
+        { provide: AuthorizationService, useClass: AuthorizationServiceMock },
+        { provide: HTTP_INTERCEPTORS, useClass: AuthorizationInterceptor, multi: true }
+      ],
+      imports: [
+        HttpClientTestingModule
       ]
-  }));
+    });
+    interceptor = TestBed.inject(AuthorizationInterceptor);
+    service = TestBed.inject(MockDataService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
 
   it('should be created', () => {
-    const interceptor: AuthorizationInterceptor = TestBed.inject(AuthorizationInterceptor);
     expect(interceptor).toBeTruthy();
   });
+
+  it('should add an Authorization header', waitForAsync(() => {
+    service.getPosts().subscribe(response => {
+      expect(response).toBeTruthy();
+      const httpRequest = httpMock.expectOne(`${service.ROOT_URL}/posts`);
+      expect(httpRequest.request.url).toBe(`${service.ROOT_URL}/posts`);
+      expect(httpRequest.request.headers.has('Authorization')).toBeTrue();
+    });
+  }));
+
+});
+
+describe('AuthorizationInterceptor', () => {
+  let interceptor: AuthorizationInterceptor;
+  let httpMock: HttpTestingController;
+  let service: MockDataService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        AuthorizationInterceptor,
+        MockDataService,
+        { provide: AuthorizationService, useClass: AuthorizationServiceMockNotAuth },
+        { provide: HTTP_INTERCEPTORS, useClass: AuthorizationInterceptor, multi: true }
+      ],
+      imports: [
+        HttpClientTestingModule
+      ]
+    });
+    interceptor = TestBed.inject(AuthorizationInterceptor);
+    service = TestBed.inject(MockDataService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  it('should not add an Authorization header', waitForAsync(() => {
+    service.getPosts().subscribe(response => {
+      expect(response).toBeTruthy();
+      const httpRequest = httpMock.expectOne(`${service.ROOT_URL}/posts`);
+      expect(httpRequest.request.headers.has('Authorization')).toBeFalse();
+    });
+ 
+  }));
+
 });
---
+++ src/app/shared/interceptors/mock-data-service.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+@Injectable()
+export class MockDataService {
+  ROOT_URL = `http://jsonplaceholder.typicode.com`;
+
+  constructor(private http: HttpClient) {}
+
+  getPosts() {
+    return this.http.get<string[]>(`${this.ROOT_URL}/posts`);
+  }
+}
