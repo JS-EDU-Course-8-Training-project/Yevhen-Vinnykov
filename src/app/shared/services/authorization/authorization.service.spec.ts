e57c8d1f1fd1d68f59aac0f28e2bec43d5ec0be3
Yevhen Vinnykov <yevhen.vinnykov@dev.pro>
2022-05-04 12:34:33 +0200

    JEC8-169 - Add Tests for Authorization Service

--- src/app/shared/services/authorization/authorization.service.spec.ts
+++ src/app/shared/services/authorization/authorization.service.spec.ts
@@ -13,4 +13,30 @@ describe('AuthorizationService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('checkIfAuthorized should make isAuthorized$ emit false ', () => {
+    const spy = spyOn(service.isAuthorized$, 'next');
+    service.checkIfAuthorized();
+    expect(spy).toHaveBeenCalledWith(false);
+  });
+
+  it('checkIfAuthorized should make isAuthorized$ emit true ', () => {
+    const spy = spyOn(service.isAuthorized$, 'next');
+    localStorage.setItem('authorized', 'true');
+    service.checkIfAuthorized();
+    expect(spy).toHaveBeenCalledWith(true);
+  });
+
+  it('authorize method should make isAuthorized$ emit true ', () => {
+    const spy = spyOn(service.isAuthorized$, 'next');
+    service.authorize('test-token');
+    expect(spy).toHaveBeenCalledWith(true);
+  });
+
+  it('removeAuthorization method should make isAuthorized$ emit false ', () => {
+    const spy = spyOn(service.isAuthorized$, 'next');
+    service.removeAuthorization();
+    expect(spy).toHaveBeenCalledWith(false);
+  });
+
 });
