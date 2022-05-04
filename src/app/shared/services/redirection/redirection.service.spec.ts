04157b531844aca1a5fd73e0fe6ef25d1c9304a3
Yevhen Vinnykov <yevhen.vinnykov@dev.pro>
2022-05-04 14:03:30 +0200

    JEC8-169 - Add Tests for Redirecton  Service

--- src/app/shared/services/redirection/redirection.service.spec.ts
+++ src/app/shared/services/redirection/redirection.service.spec.ts
@@ -1,16 +1,34 @@
-import { TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
 
 import { RedirectionService } from './redirection.service';
 
 describe('RedirectionService', () => {
   let service: RedirectionService;
+  let routerSpy: jasmine.SpyObj<Router>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(RedirectionService);
+    routerSpy = jasmine.createSpyObj('Router', ['navigateByUrl']);
+    service = new RedirectionService(routerSpy);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('methods should return a promise that resolves', () => {
+    routerSpy.navigateByUrl.and.returnValue(new Promise<boolean>((resolve, reject) => resolve(true)));
+    service.redirectUnauthorized();
+    service.redirectToEditArticle('test-slug');
+    service.redirectHome();
+    service.redirectByUrl('/test-url');
+    expect(routerSpy.navigateByUrl.calls.count()).toBe(4);
   });
+
+  it('methods should return a promise that rejects', () => {
+    routerSpy.navigateByUrl.and.returnValue(new Promise<boolean>((resolve, reject) => reject('Error')));
+    service.redirectUnauthorized();
+    service.redirectByUrl('/test-url');
+    service.redirectHome();
+    service.redirectToEditArticle('test-slug');
+    expect(routerSpy.navigateByUrl.calls.count()).toBe(4);
+  });
+
 });
+
+
