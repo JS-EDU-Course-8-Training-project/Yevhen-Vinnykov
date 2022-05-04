e94c56bce1f5d8b1aa806220af017d3d2871cabf
Yevhen Vinnykov <yevhen.vinnykov@dev.pro>
2022-05-04 12:50:29 +0200

    JEC8-169 - Add Tests for Profiles Service

--- src/app/shared/services/profiles/profiles.service.spec.ts
+++ src/app/shared/services/profiles/profiles.service.spec.ts
@@ -1,16 +1,81 @@
-import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { IProfile } from '../../models/IProfile';
 
 import { ProfilesService } from './profiles.service';
 
-describe('ProfilesService', () => {
+const mockProfile: IProfile = {
+  username: 'test-username',
+  bio: 'test-bio',
+  image: 'test-image',
+  following: false
+};
+
+describe('ProfilesService Get Method', () => {
   let service: ProfilesService;
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ProfilesService);
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    service = new ProfilesService(httpClientSpy);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('fetchUser methods should return expected data', (done: DoneFn) => {
+    httpClientSpy.get.and.returnValue(of({ profile: mockProfile }));
+    service.fetchUser('test-username').subscribe({
+      next: user => {
+        expect(user).withContext('expected user').toEqual(mockProfile);
+        done();
+      },
+      error: done.fail
+    });
+    expect(httpClientSpy.get.calls.count()).toBe(1);
   });
+
+});
+
+describe('ProfilesService Post Method', () => {
+  let service: ProfilesService;
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
+
+  beforeEach(() => {
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['post']);
+    service = new ProfilesService(httpClientSpy);
+  });
+
+  it('follow methods should return expected data', (done: DoneFn) => {
+    httpClientSpy.post.and.returnValue(of({ profile: {...mockProfile, following: true} }));
+    service.follow('test-username').subscribe({
+      next: user => {
+        expect(user).withContext('expected user').toEqual({...mockProfile, following: true});
+        done();
+      },
+      error: done.fail
+    });
+    expect(httpClientSpy.post.calls.count()).toBe(1);
+  });
+
+});
+
+describe('ProfilesService Delete Method', () => {
+  let service: ProfilesService;
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
+
+  beforeEach(() => {
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['delete']);
+    service = new ProfilesService(httpClientSpy);
+  });
+
+  it('unfollow methods should return expected data', (done: DoneFn) => {
+    httpClientSpy.delete.and.returnValue(of({ profile: {...mockProfile, following: false} }));
+    service.unfollow('test-username').subscribe({
+      next: user => {
+        expect(user).withContext('expected user').toEqual({...mockProfile, following: false});
+        done();
+      },
+      error: done.fail
+    });
+    expect(httpClientSpy.delete.calls.count()).toBe(1);
+  });
+
 });
