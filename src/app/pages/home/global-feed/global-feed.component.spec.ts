f26a31db7ef39b852082a02c73f6bf102fd6ef41
vladyslav.kiskin <vladyslav.kiskin@dev.pro>
2022-05-10 20:08:50 +0300

    implement tests for global feed

--- src/app/pages/home/global-feed/global-feed.component.spec.ts
+++ src/app/pages/home/global-feed/global-feed.component.spec.ts
@@ -1,10 +1,12 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { ArticlesService } from './../../../shared/services/articles/articles.service';
 import { IArticleResponse } from './../../../shared/models/IArticle';
-import { of, throwError } from 'rxjs';
+import {catchError, of, tap, throwError} from 'rxjs';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { GlobalFeedComponent } from './global-feed.component';
+import {InfiniteScrollService} from "../../../shared/services/infinite-scroll/infinite-scroll.service";
+import {ElementRef, NO_ERRORS_SCHEMA, QueryList} from "@angular/core";
 
 const expectedData: IArticleResponse = {
   articles: [
@@ -38,115 +40,131 @@ class ArticlesServiceMockWithError {
   public fetchArticles = (offset: number, limit: number) => throwError(() => HttpErrorResponse);
 }
 
+class InfiniteScrollServiceMock {
+  public observeIntersection = () => of([]);
+  public observer = {observe: () => {}}
+}
+
 describe('GlobalFeedComponent', () => {
-  let component: GlobalFeedComponent;
-  let fixture: ComponentFixture<GlobalFeedComponent>;
+  describe('When no error thrown', () => {
+    let component: GlobalFeedComponent;
+    let fixture: ComponentFixture<GlobalFeedComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [GlobalFeedComponent],
-      providers: [
-        { provide: ArticlesService, useClass: ArticlesServiceMock }
-      ]
-    })
-      .compileComponents();
+    beforeEach(async () => {
+      await TestBed.configureTestingModule({
+        declarations: [GlobalFeedComponent],
+        providers: [
+          { provide: ArticlesService, useClass: ArticlesServiceMock },
+          { provide: InfiniteScrollService, useClass: InfiniteScrollServiceMock }
+        ],
+        schemas: [NO_ERRORS_SCHEMA]
+      })
+        .compileComponents();
+    });
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(GlobalFeedComponent);
+      component = fixture.componentInstance;
+      component.tabIndex = 1;
+      component.isAuthorized = true;
+      fixture.detectChanges();
+      component.ngOnChanges();
+    });
+
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+
+    it('setDataOnResponse should be called because tabIndex is 1', waitForAsync(() => {
+      const service = TestBed.inject(ArticlesService);
+      service.fetchArticles().subscribe((data: IArticleResponse) => {
+        expect(data.articles).toEqual(expectedData.articles);
+      })
+    }));
+
+    it('setDataOnResponse should be called because is Authorized is false', waitForAsync(() => {
+      component.isAuthorized = false;
+      component.tabIndex = 0;
+      const service = TestBed.inject(ArticlesService);
+      service.fetchArticles().subscribe((data: IArticleResponse) => {
+        expect(data.articles).toEqual(expectedData.articles);
+      })
+    }));
+
   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(GlobalFeedComponent);
-    component = fixture.componentInstance;
-    component.tabIndex = 1;
-    component.isAuthorized = true;
-    fixture.detectChanges();
+
+  describe('When throw error', () => {
+    let component: GlobalFeedComponent;
+    let fixture: ComponentFixture<GlobalFeedComponent>;
+
+    beforeEach(async () => {
+      await TestBed.configureTestingModule({
+        declarations: [GlobalFeedComponent],
+        providers: [
+          { provide: ArticlesService, useClass: ArticlesServiceMockWithError },
+          { provide: InfiniteScrollService, useClass: InfiniteScrollServiceMock }
+
+        ],
+        schemas: [NO_ERRORS_SCHEMA]
+
+      })
+        .compileComponents();
+    });
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(GlobalFeedComponent);
+      component = fixture.componentInstance;
+      component.tabIndex = 1;
+      component.isAuthorized = false;
+      component.lastItem = new QueryList<ElementRef<any>>();
+      fixture.detectChanges();
+      component.ngOnChanges();
+    });
+
+    it('onCatchError should be called', waitForAsync(() => {
+      const service = TestBed.inject(ArticlesService);
+      service.fetchArticles().pipe(
+        catchError(() => {
+          expect(component.error).toBe('Something went wrong :(')
+          return of('')
+        })
+      )
+    }));
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-
-  it('setDataOnResponse should be called because tabIndex is 1', () => {
-    const spy = spyOn<any>(component, 'setDataOnResponse').and.callThrough();
-    component.ngOnChanges();
-    expect(spy).toHaveBeenCalledWith(expectedData);
-  });
-
-  it('setDataOnResponse should be called because is Authorized is false', () => {
-    component.isAuthorized = false;
-    component.tabIndex = 0;
-    const spy = spyOn<any>(component, 'setDataOnResponse').and.callThrough();
-    component.ngOnChanges();
-    expect(spy).toHaveBeenCalledWith(expectedData);
-  });
-
-});
-
-
-// describe('GlobalFeedComponent', () => {
-//   let component: GlobalFeedComponent;
-//   let fixture: ComponentFixture<GlobalFeedComponent>;
-
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       declarations: [GlobalFeedComponent],
-//       providers: [
-//         { provide: ArticlesService, useClass: ArticlesServiceMockWithError },
-//         { provide: InfiniteScrollService, useClass: InfiniteScrollServiceMock }
-
-//       ]
-//     })
-//       .compileComponents();
-//   });
-
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(GlobalFeedComponent);
-//     component = fixture.componentInstance;
-//     component.tabIndex = 1;
-//     component.isAuthorized = false;
-//     component.lastItem = new QueryList<ElementRef<any>>();
-//     fixture.detectChanges();
-//   });
-
-//   it('onCatchError should be called', () => {
-//     const spy = spyOn<any>(component, 'onCatchError').and.callThrough();
-//     component.ngOnChanges();
-//     expect(spy).toHaveBeenCalled();
-//   });
-
-// });
-
-
-// describe('GlobalFeedComponent', () => {
-//   let component: GlobalFeedComponent;
-//   let fixture: ComponentFixture<GlobalFeedComponent>;
-//   let articleServiceSpy: jasmine.SpyObj<ArticlesService>;
-
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       declarations: [GlobalFeedComponent],
-//       providers: [
-//         { provide: ArticlesService, useClass: ArticlesServiceMockWithError },
-//       ]
-//     })
-//       .compileComponents();
-//   });
-
-//   beforeEach(() => {
-//     articleServiceSpy = jasmine.createSpyObj('ArticlesService', ['fetchArticles']);
-//     fixture = TestBed.createComponent(GlobalFeedComponent);
-//     component = fixture.componentInstance;
-//     component.tabIndex = 1;
-//     component.isAuthorized = false;
-//     fixture.detectChanges();
-//   });
-
-//   it('onCatchError should be called', () => {
-//     articleServiceSpy.fetchArticles.and.returnValue(throwError(() => Error('Fetching articles failed')));
-//     fixture.detectChanges();
-//     const spy = spyOn<any>(component, 'onCatchError').and.callThrough();
-//     component.ngOnChanges();
-//     fixture.detectChanges();
-//     expect(spy).toHaveBeenCalled();
-//     expect(component.error).toBe('Something went wrong :(');
-//   });
-
-// });
+  // I think test below is extra
+  // describe('GlobalFeedComponent', () => {
+  //   let component: GlobalFeedComponent;
+  //   let fixture: ComponentFixture<GlobalFeedComponent>;
+  //   let articleServiceSpy: jasmine.SpyObj<ArticlesService>;
+  //
+  //   beforeEach(async () => {
+  //     await TestBed.configureTestingModule({
+  //       declarations: [GlobalFeedComponent],
+  //       providers: [
+  //         { provide: ArticlesService, useClass: ArticlesServiceMockWithError },
+  //       ]
+  //     })
+  //       .compileComponents();
+  //   });
+  //
+  //   beforeEach(() => {
+  //     articleServiceSpy = jasmine.createSpyObj('ArticlesService', ['fetchArticles']);
+  //     fixture = TestBed.createComponent(GlobalFeedComponent);
+  //     component = fixture.componentInstance;
+  //     component.tabIndex = 1;
+  //     component.isAuthorized = false;
+  //     fixture.detectChanges();
+  //   });
+  //
+  //   it('onCatchError should be called', () => {
+  //     articleServiceSpy.fetchArticles.and.returnValue(throwError(() => Error('Fetching articles failed')));
+  //     fixture.detectChanges();
+  //     const spy = spyOn<any>(component, 'onCatchError').and.callThrough();
+  //     fixture.detectChanges();
+  //     expect(spy).toHaveBeenCalled();
+  //     expect(component.error).toBe('Something went wrong :(');
+  //   });
+  //
+  // });
+})
