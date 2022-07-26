2cf15d60a9c3dda2226f138585176a603b199c71
Yevhen Vinnykov <yevhen.vinnykov@dev.pro>
2022-07-26 21:44:54 +0200

    Add Global Articles Store And Refactor

--- src/app/pages/home/global-feed/global-feed.component.ts
+++ src/app/pages/home/global-feed/global-feed.component.ts
@@ -1,5 +1,4 @@
-import { BehaviorSubject, Subject, takeUntil } from 'rxjs';
-import { IArticle, IArticleResponse } from 'src/app/shared/models/IArticle';
+import { Subject, takeUntil } from 'rxjs';
 import {
   Component,
   OnChanges,
@@ -9,16 +8,16 @@ import {
   QueryList,
   AfterViewInit,
   ChangeDetectionStrategy,
-  ChangeDetectorRef,
 } from '@angular/core';
-import { ArticlesService } from 'src/app/shared/services/articles/articles.service';
-import { InfiniteScrollService } from 'src/app/shared/services/infinite-scroll/infinite-scroll.service';
+import { InfiniteScrollService } from 'src/app/shared/services/infinite-scroll/new-infinite-scroll.service';
 import { TestedComponent } from 'src/app/shared/tests/TestedComponent';
+import { GlobalArticlesStore } from './global-feed.store';
 
 @Component({
   selector: 'app-global-feed',
   templateUrl: './global-feed.component.html',
   styleUrls: ['./global-feed.component.scss'],
+  providers: [GlobalArticlesStore, InfiniteScrollService],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class GlobalFeedComponent
@@ -30,93 +29,35 @@ export class GlobalFeedComponent
   @Input() tabIndex!: number;
   @Input() isAuthorized!: boolean;
 
-  public globalArticles: IArticle[] = [];
-  public isLoading = false;
   private notifier: Subject<void> = new Subject<void>();
-  public isFinished!: boolean;
-  private offset = 0;
-  private currentPage = 1;
-  private pagesTotalCount!: number;
-  private limit = 5;
-  public canLoad$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(
-    true
-  );
-  public error = '';
 
   constructor(
-    private articlesService: ArticlesService,
-    private infiniteScroll: InfiniteScrollService,
-    private cdRef: ChangeDetectorRef
+    public store: GlobalArticlesStore,
+    private scrollService: InfiniteScrollService
   ) {
     super();
   }
 
   ngOnChanges(): void {
-    this.reset();
+    this.store.reset();
+
     if (this.tabIndex === 1 || !this.isAuthorized) {
-      this.getArticles();
-      this.infiniteScroll.observeIntersection({
-        canLoad: this.canLoad$,
-        callback: this.getArticles.bind(this),
-      });
+      this.store.getArticles();
+      this.scrollService.initObserver(() => this.store.getArticles());
     }
+  }
+
+  ngAfterViewInit(): void {
+    this.lastItem.changes.pipe(takeUntil(this.notifier)).subscribe((change) => {
+      if (change.last && !this.store.loadedAllArticles) {
+        this.scrollService.observer.observe(change.last.nativeElement);
+      }
+    });
   }
 
   ngOnDestroy(): void {
     this.notifier.next();
     this.notifier.complete();
-  }
-
-  ngAfterViewInit(): void {
-    this.lastItem.changes.pipe(takeUntil(this.notifier)).subscribe((change) => {
-      if (change.last)
-        this.infiniteScroll.observer.observe(change.last.nativeElement);
-    });
-  }
-
-  private async getArticles(): Promise<void> {
-    this.error = '';
-    this.isLoading = true;
-    this.cdRef.detectChanges();
-
-    try {
-      const articlesResponse = await this.articlesService.fetchArticles(
-        this.offset,
-        this.limit
-      );
-      this.setDataOnResponse(articlesResponse);
-    } catch (error) {
-      this.error = error as string;
-    } finally {
-      this.isLoading = false;
-      this.cdRef.detectChanges();      
-    }
-  }
-
-  private setDataOnResponse(res: IArticleResponse): void {
-    this.globalArticles = [...this.globalArticles, ...res.articles];
-    this.pagesTotalCount = Math.ceil(res.articlesCount / this.limit);
-    this.isFinished = this.currentPage === this.pagesTotalCount;
-
-    this.canLoad$.next(!this.isFinished);
-    this.nextPage();
-
-    this.cdRef.detectChanges();
-  }
-
-  private nextPage(): void {
-    if (this.currentPage < this.pagesTotalCount) {
-      this.currentPage++;
-      this.offset += this.limit;
-    }
-  }
-
-  private reset(): void {
-    this.globalArticles = [];
-    this.offset = 0;
-    this.pagesTotalCount = 0;
-    this.isFinished = false;
-    this.currentPage = 1;
-    this.cdRef.detectChanges();
+    this.scrollService.observer?.disconnect();
   }
 }
---
+++ src/app/pages/home/global-feed/global-feed.store.ts
@@ -0,0 +1,71 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { IArticle } from 'src/app/shared/models/IArticle';
+import { ArticlesService } from 'src/app/shared/services/articles/articles.service';
+
+@Injectable()
+export class GlobalArticlesStore {
+  readonly articles$ = new BehaviorSubject<IArticle[]>([]);
+  readonly isLoading$ = new BehaviorSubject<boolean>(false);
+  readonly loadedAllArticles$ = new BehaviorSubject<boolean>(false);
+  readonly error$ = new BehaviorSubject<string>('');
+
+  private offset = 0;
+  private currentPage = 1;
+  private pagesTotalCount = 1;
+  private limit = 5;
+
+  get articles(): IArticle[] {
+    return this.articles$.getValue();
+  }
+
+  get loadedAllArticles(): boolean {
+    return this.loadedAllArticles$.getValue();
+  }
+
+  get error(): string {
+    return this.error$.getValue();
+  }
+
+  constructor(private articlesService: ArticlesService) {}
+
+  public async getArticles(): Promise<void> {
+    if (this.loadedAllArticles$.getValue()) return;
+
+    this.error$.next('');
+    this.isLoading$.next(true);
+
+    try {
+      const { articles, articlesCount } =
+        await this.articlesService.fetchArticles(this.offset, this.limit);
+
+      this.handleResponse(articles, articlesCount);
+    } catch (error) {
+      this.error$.next(error as string);
+    } finally {
+      this.isLoading$.next(false);
+    }
+  }
+
+  private handleResponse(articles: IArticle[], articlesCount: number): void {
+    this.pagesTotalCount = Math.ceil(articlesCount / this.limit);
+    this.articles$.next([...this.articles$.getValue(), ...articles]);
+
+    if (this.currentPage === this.pagesTotalCount) {
+      this.loadedAllArticles$.next(true);
+      return;
+    }
+
+    this.currentPage++;
+    this.offset += this.limit;
+  }
+
+  public reset(): void {
+    this.articles$.next([]);
+    this.loadedAllArticles$.next(false);
+
+    this.offset = 0;
+    this.currentPage = 1;
+    this.pagesTotalCount = 1;
+  }
+}
