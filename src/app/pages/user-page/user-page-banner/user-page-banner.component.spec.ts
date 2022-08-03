bf8620a408c5e62698ba723e849b104bafcaeb14
Yevhen Vinnykov <yevhen.vinnykov@dev.pro>
2022-08-03 21:59:50 +0200

    Fix User Page  Unit Tests

--- src/app/pages/user-page/user-page-banner/user-page-banner.component.spec.ts
+++ src/app/pages/user-page/user-page-banner/user-page-banner.component.spec.ts
@@ -1,6 +1,35 @@
+import { IExistingUser } from './../../../shared/models/IExistingUser';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AuthorizationService } from 'src/app/shared/services/authorization/authorization.service';
 
 import { UserPageBannerComponent } from './user-page-banner.component';
+import { of } from 'rxjs';
+import { ProfilesService } from 'src/app/shared/services/profiles/profiles.service';
+
+const userMock: IExistingUser = {
+  id: '1',
+  email: 'test-user@example.com',
+  password: 'test-password',
+  image: '',
+  username: 'John',
+};
+
+class AuthServiceMock {
+  public authUser$ = { getValue: () => userMock };
+}
+
+class RouterMock {
+  public events = of(NavigationEnd);
+}
+
+class RouteMock {
+  public snapshot = { params: { 'user-name': 'John' } };
+}
+
+class ProfilesServiceMock {
+  public fetchUser = () => new Promise((resolve) => resolve(userMock));
+}
 
 describe('UserPageBannerComponent', () => {
   let component: UserPageBannerComponent;
@@ -8,9 +37,14 @@ describe('UserPageBannerComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UserPageBannerComponent ]
-    })
-    .compileComponents();
+      declarations: [UserPageBannerComponent],
+      providers: [
+        { provide: AuthorizationService, useClass: AuthServiceMock },
+        { provide: Router, useClass: RouterMock },
+        { provide: ActivatedRoute, useClass: RouteMock },
+        { provide: ProfilesService, useClass: ProfilesServiceMock },
+      ],
+    }).compileComponents();
   });
 
   beforeEach(() => {
--- src/app/pages/user-page/user-page.component.spec.ts
+++ src/app/pages/user-page/user-page.component.spec.ts
@@ -1,61 +1,52 @@
-import { IProfile } from './../../shared/models/IProfile';
-import { ProfilesService } from './../../shared/services/profiles/profiles.service';
-import { IExistingUser } from './../../shared/models/IExistingUser';
+import { IArticleResponse } from 'src/app/shared/models/IArticle';
 import { of } from 'rxjs';
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserPageComponent } from './user-page.component';
-import { NavigationEnd, Router } from '@angular/router';
-import { RedirectionService } from 'src/app/shared/services/redirection/redirection.service';
-import { By } from '@angular/platform-browser';
-import { AuthorizationService } from 'src/app/shared/services/authorization/authorization.service';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
-import { TestAttributes } from 'src/app/shared/tests/TestAttributes';
 import { TestAttributeDirective } from 'src/app/shared/tests/test-attribute.directive';
-
-const mockAuthUser: IExistingUser = {
-  id: '1',
-  email: 'test-auth@example.com',
-  username: 'test-auth-username',
-  bio: 'test-auth-bio',
-  image: 'test-auth-image',
-  token: 'test-auth--token',
-  password: 'test-auth-password',
-};
-
-const mockUser: IProfile = {
-  username: 'test-username',
-  bio: '',
-  image: 'test-image',
-  following: false,
-};
-
-class ProfilesServiceMock {
-  public follow = () => of({ following: true });
-
-  public unfollow = () => of({ following: false });
-
-  public fetchUser = () => of(mockUser);
-}
+import { ArticlesService } from 'src/app/shared/services/articles/articles.service';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 class RouterMock {
   public url = 'localhost:4200/user/test-username';
   public events = of(NavigationEnd);
 }
 
-class RedirectionServiceMock {
-  public redirectUnauthorized = () =>
-    new Promise<boolean>((resolve) => resolve(true));
+class RouteMock {
+  public snapshot = { params: { 'user-name': 'John' } };
 }
 
-class AuthorizationServiceMock {
-  public isAuthorized$ = of(true);
-  public authUser$ = of(mockAuthUser);
-}
+const dataMock: IArticleResponse = {
+  articles: [
+    {
+      id: '1',
+      slug: 'test-slug',
+      title: 'test-title',
+      description: 'test-description',
+      body: 'test-body',
+      image: 'test-image',
+      tagList: ['test-tag'],
+      createdAt: Date.now().toString(),
+      updatedAt: Date.now().toString(),
+      favorited: false,
+      favoritesCount: 2,
+      author: {
+        username: 'test-author',
+        bio: 'test-bio',
+        image: 'test-image',
+        following: false,
+      },
+    },
+  ],
+  articlesCount: 10,
+};
 
-class AuthorizationServiceMockNotAuth {
-  public isAuthorized$ = of(false);
-  public authUser$ = of({});
+class ArticlesServiceMock {
+  public fetchUserArticles = () => new Promise((resolve) => resolve(dataMock));
+  public fetchFavoritedArticles = () =>
+    new Promise((resolve) => resolve(dataMock));
 }
 
 describe('USER PAGE COMPONENT', () => {
@@ -65,11 +56,11 @@ describe('USER PAGE COMPONENT', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [UserPageComponent, TestAttributeDirective],
+      imports: [MatSnackBarModule],
       providers: [
-        { provide: ProfilesService, useClass: ProfilesServiceMock },
         { provide: Router, useClass: RouterMock },
-        { provide: RedirectionService, useClass: RedirectionServiceMock },
-        { provide: AuthorizationService, useClass: AuthorizationServiceMock },
+        { provide: ActivatedRoute, useClass: RouteMock },
+        { provide: ArticlesService, useClass: ArticlesServiceMock },
       ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
     }).compileComponents();
@@ -80,122 +71,8 @@ describe('USER PAGE COMPONENT', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should follow a user', waitForAsync(() => {
-    const spy = spyOn<any>(component, 'followingHandler').and.callThrough();
-    const followButton = fixture.debugElement.query(
-      By.css(`[data-test=${TestAttributes.UserPageFollowBtn}]`)
-    );
-
-    expect(followButton.nativeElement.innerText).toBe('Follow test-username');
-    expect(component.isFollowed).toBe(false);
-
-    followButton.triggerEventHandler('click', null);
-    fixture.detectChanges();
-
-    fixture.whenStable().then(() => {
-      expect(component.isFollowed).toBe(true);
-      expect(followButton.nativeElement.innerText).toBe(
-        'Unfollow test-username'
-      );
-      expect(spy).toHaveBeenCalledWith('test-username', 'follow');
-    });
-  }));
-
-  it('should unfollow a user', waitForAsync(() => {
-    const spy = spyOn<any>(component, 'followingHandler').and.callThrough();
-    const followButton = fixture.debugElement.query(
-      By.css(`[data-test=${TestAttributes.UserPageFollowBtn}]`)
-    );
-
-    followButton.triggerEventHandler('click', null);
-
-    expect(component.isFollowed).toBe(true);
-
-    followButton.triggerEventHandler('click', null);
-    fixture.detectChanges();
-
-    fixture.whenStable().then(() => {
-      expect(component.isFollowed).toBe(false);
-      expect(followButton.nativeElement.innerText).toBe('Follow test-username');
-      expect(spy).toHaveBeenCalledWith('test-username', 'unfollow');
-    });
-  }));
-});
-
-describe('USER PAGE > REDIRECT UNAUTHORIZED', () => {
-  let component: UserPageComponent;
-  let fixture: ComponentFixture<UserPageComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [UserPageComponent, TestAttributeDirective],
-      providers: [
-        { provide: ProfilesService, useClass: ProfilesServiceMock },
-        { provide: Router, useClass: RouterMock },
-        { provide: RedirectionService, useClass: RedirectionServiceMock },
-        {
-          provide: AuthorizationService,
-          useClass: AuthorizationServiceMockNotAuth,
-        },
-      ],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
-    }).compileComponents();
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(UserPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should be invoked', () => {
-    const spy = spyOn<any>(component, 'redirectUnauthorized');
-    const followButton = fixture.debugElement.query(
-      By.css(`[data-test=${TestAttributes.UserPageFollowBtn}]`)
-    );
-
-    followButton.triggerEventHandler('click', null);
-    fixture.detectChanges();
-
-    expect(spy).toHaveBeenCalled();
-  });
-});
-
-describe('USER PAGE > MYSELF MODE', () => {
-  let component: UserPageComponent;
-  let fixture: ComponentFixture<UserPageComponent>;
-
-  class RouterMock {
-    public url = 'localhost:4200/user/test-auth-username';
-    public events = of(NavigationEnd);
-  }
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [UserPageComponent],
-      providers: [
-        { provide: ProfilesService, useClass: ProfilesServiceMock },
-        { provide: Router, useClass: RouterMock },
-        { provide: RedirectionService, useClass: RedirectionServiceMock },
-        { provide: AuthorizationService, useClass: AuthorizationServiceMock },
-      ],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
-    }).compileComponents();
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(UserPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should initialize correctly', () => {
-    const followButton = fixture.debugElement.query(
-      By.css(`[data-test=${TestAttributes.UserPageFollowBtn}]`)
-    );
-
-    expect(component.isMyself).toBe(true);
-    expect(followButton).toBeFalsy();
+  
+  it('should create', () => {
+    expect(component).toBeTruthy();
   });
 });
